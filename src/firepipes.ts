1bec99053135d3f9d0d5cc8e4c6c5693c3457112
Dimitri Rosenberg <di@radiant.fm>
2015-11-24 20:24:17 +0300

    add core pipe ToArrayPipe that transforms Firebase snapshot to ordered array with assigned meta

--- src/firepipes.ts
+++ src/firepipes.ts
@@ -1,10 +1,13 @@
 import { OnValuePipe } from './on_value_pipe'
+import { ToArrayPipe } from './to_array_pipe'
 import { ToObjectPipe } from './to_object_pipe'
 
 export { OnValuePipe } from './on_value_pipe'
+export { ToArrayPipe } from './to_array_pipe'
 export { ToObjectPipe } from './to_object_pipe'
 
 export const FIREBASE_PIPES = [
   OnValuePipe,
+  ToArrayPipe,
   ToObjectPipe,
 ];
