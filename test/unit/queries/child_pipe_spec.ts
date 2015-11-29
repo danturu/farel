3ab48021a720d9f7a6e292ed6566a988947855b3
Dimitri Rosenberg <di@radiant.fm>
2015-11-29 20:56:24 +0300

    feat(query): add ChildPipe

---
+++ test/unit/queries/child_pipe_spec.ts
@@ -0,0 +1,11 @@
+import * as Firebase from 'firebase'
+
+import { ChildPipe } from '../../../src/queries/child_pipe'
+
+export function main(firebaseUrl: string) {
+  describe("ChildPipe", () => {
+    it("should be tested", () => {
+      pending();
+    });
+  });
+}
