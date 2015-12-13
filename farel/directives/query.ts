647207fd55bc3f9b7801744ff4b646f9d56c5844
Dimitri Rosenberg <dimitri@range-app.com>
2015-12-14 00:30:25 +0300

    refactor(directives): wrap query result to $value

--- farel/directives/query.ts
+++ farel/directives/query.ts
@@ -1,19 +1,15 @@
-import { Directive } from 'angular2/core'
+import { Directive } from 'angular2/core';
 
 @Directive({
   selector: '[query]', exportAs: 'query', inputs: ['result: query'],
 })
 
 export class Query {
-  set result(val: any) {
-    if (val === null || val === undefined) {
-      return;
-    }
-
-    if (typeof val === 'object' && !Array.isArray(val)) {
-      Object.keys(val).forEach(key => this[key] = val[key]);
+  set result(value: any) {
+    if (value) {
+      this['$value'] = value;
     } else {
-      this['$value'] = val;
+      delete this['$value'];
     }
   }
 }
